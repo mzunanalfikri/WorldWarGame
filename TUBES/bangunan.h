6ceaad780171c3e9766dcaee15416904412f6528
mzunana <zunanalfikri@gmail.com>
2019-10-26 06:38:55 +0700

    ADT

---
+++ TUBES/bangunan.h
@@ -0,0 +1,36 @@
+/*
+Properti ADT Bangunan :
+1. Id
+    Kepemilikan {tidak perllu, sudah ada di list}
+2. Type
+3. Jumlah Pasukan
+4. Level
+5. A (Nilai Penambahan Pasukan)
+6. M (Maksimum Penambahan Pasukan)
+7. P (Pertahanan)
+     U (Pasukan Awal) {tidak perlu, karena hanya di awal}
+8. Posisi
+*/
+
+#ifndef BANGUNAN_H
+#define BANGUNAN_H
+
+#include "boolean.h"
+#include "point.h"
+
+typedef struct {
+    int Id;
+    char Type;
+    int Pasukan;
+    int Level;
+    int A;
+    int M;
+    boolean P;
+    POINT Posisi;
+} Bangunan;
+
+/* Akses dengan Selektor */
+#define 
+
+
+#endif
