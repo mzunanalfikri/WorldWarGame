387f89cb509db646047c3e7d0f225cd8208859b0
mzunana <zunanalfikri@gmail.com>
2019-10-26 09:52:52 +0700

    ADT Bangunan

---
+++ TUBES/bangunan.c
@@ -0,0 +1,130 @@
+#include "bangunan.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+void MakeBangunan (Bangunan * B, char type, POINT P)
+/* I.S. Sembarang */
+/* F.S. Membuat sebuah bangunan default level 1 (digunakan pada awal load file) */
+{
+    Level(*B) = 1;
+    Serang(*B) = false;
+    Pertahanan(*B) = false;
+    Type(*B) = type;
+    Posisi(*B) = P;
+    if (type == 'C'){
+        Pasukan(*B) = 40;
+        A(*B) = 10;
+        M(*B) = 40;
+    } else if (type == 'T') {
+        Pasukan(*B) = 30;
+        A(*B) = 5;
+        M(*B) = 20;
+        Pertahanan(*B) = true;
+    } else if (type == 'F') {
+        Pasukan(*B) = 80;
+        A(*B) = 10;
+        M(*B) = 20;
+    } else if (type == 'V'){
+        Pasukan(*B) = 20;
+        A(*B) = 5;
+        M(*B) = 20;
+    }
+}
+
+boolean IsNaikLevel (Bangunan B)
+/* Mengirim True jika bangunan siap naik level */
+{
+    return (Pasukan(B) >= M(B)/2 && Level(B)<4);
+}
+
+void NaikLevel (Bangunan *B)
+/* I.S Bangunan valid untuk naik level (bisa naik level) */
+/* F.S Bangunan naik 1 level dengan pertambahan 
+        pasukan sesuai dengan levelnya */
+{
+    if (Type(*B) == 'C'){
+        if (Level(*B) == 1) {
+            Pasukan(*B) = Pasukan(*B) - M(*B)/2;
+            A(*B) = 15;
+            M(*B) = 60;
+        } else if (Level(*B) == 2) {
+            Pasukan(*B) = Pasukan(*B) - M(*B)/2;
+            A(*B) = 20;
+            M(*B) = 80;
+        } else if (Level(*B) == 3) {
+            Pasukan(*B) = Pasukan(*B) - M(*B)/2;
+            A(*B) = 25;
+            M(*B) = 100;
+        }
+    } else if (Type(*B) == 'T'){
+        if (Level(*B) == 1) {
+            Pasukan(*B) = Pasukan(*B) - M(*B)/2;
+            A(*B) = 10;
+            M(*B) = 30;
+        } else if (Level(*B) == 2) {
+            Pasukan(*B) = Pasukan(*B) - M(*B)/2;
+            A(*B) = 20;
+            M(*B) = 40;
+        } else if (Level(*B) == 3) {
+            Pasukan(*B) = Pasukan(*B) - M(*B)/2;
+            A(*B) = 30;
+            M(*B) = 50;
+        }
+    } else if (Type(*B) == 'F'){
+        if (Level(*B) == 1) {
+            Pasukan(*B) = Pasukan(*B) - M(*B)/2;
+            A(*B) = 20;
+            M(*B) = 40;
+        } else if (Level(*B) == 2) {
+            Pasukan(*B) = Pasukan(*B) - M(*B)/2;
+            A(*B) = 30;
+            M(*B) = 60;
+            Pertahanan(*B) = true;
+        } else if (Level(*B) == 3) {
+            Pasukan(*B) = Pasukan(*B) - M(*B)/2;
+            A(*B) = 40;
+            M(*B) = 80;
+        }
+    } else if (Type(*B) == 'V'){
+        if (Level(*B) == 1) {
+            Pasukan(*B) = Pasukan(*B) - M(*B)/2;
+            A(*B) = 10;
+            M(*B) = 30;
+        } else if (Level(*B) == 2) {
+            Pasukan(*B) = Pasukan(*B) - M(*B)/2;
+            A(*B) = 15;
+            M(*B) = 40;
+        } else if (Level(*B) == 3) {
+            Pasukan(*B) = Pasukan(*B) - M(*B)/2;
+            A(*B) = 20;
+            M(*B) = 50;
+        }
+    }
+    Level(*B)+=1;
+}
+
+void AddNextTurn (Bangunan * B)
+/* I.S Bangunan terdefinisi */
+/* F.S Bangunan betambah pasukannya sesuai level seteah turn */
+{
+    if (Pasukan(*B) < M(*B)){
+        Pasukan(*B) += A(*B);
+    }
+}
+
+void PrintBangunan (Bangunan B)
+/* I.S Sembarang */
+/* F.S Tercetak di layar info bangunannya */
+{
+    if (Type(B) == 'C' ) {
+        printf("Castle (%d,%d) %d lv. %d\n", Absis(Posisi(B)), Ordinat(Posisi(B)), Pasukan(B), Level(B));
+    } else if (Type(B) == 'T') {
+        printf("Tower (%d,%d) %d lv. %d\n", Absis(Posisi(B)), Ordinat(Posisi(B)), Pasukan(B), Level(B));
+    } else if (Type(B) == 'F') {
+        printf("Fort (%d,%d) %d lv. %d\n", Absis(Posisi(B)), Ordinat(Posisi(B)), Pasukan(B), Level(B));
+    } else if (Type(B) == 'V') {
+        printf("Village (%d,%d) %d lv. %d\n", Absis(Posisi(B)), Ordinat(Posisi(B)), Pasukan(B), Level(B));
+    }
+}
--- TUBES/bangunan.h
+++ TUBES/bangunan.h
@@ -1,6 +1,6 @@
 /*
 Properti ADT Bangunan :
-1. Id
+    Id
     Kepemilikan {tidak perllu, sudah ada di list}
 2. Type
 3. Jumlah Pasukan
@@ -10,6 +10,8 @@ Properti ADT Bangunan :
 7. P (Pertahanan)
      U (Pasukan Awal) {tidak perlu, karena hanya di awal}
 8. Posisi
+9. Serang (false jika belom menyerang, true jika sudah menyerang)
+10. Netral (true jika bangunan tidak dimiliki siapapun)
 */
 
 #ifndef BANGUNAN_H
@@ -19,18 +21,47 @@ Properti ADT Bangunan :
 #include "point.h"
 
 typedef struct {
-    int Id;
     char Type;
     int Pasukan;
     int Level;
     int A;
     int M;
-    boolean P;
+    boolean Pertahanan;
     POINT Posisi;
+    boolean Serang;
+    boolean Netral;
 } Bangunan;
 
 /* Akses dengan Selektor */
-#define 
+#define Type(B) (B).Type
+#define Pasukan(B) (B).Pasukan
+#define Level(B) (B).Level
+#define A(B) (B).A
+#define M(B) (B).M
+#define Pertahanan(B) (B).Pertahanan
+#define Posisi(B) (B).Posisi
+#define Serang(B) (B).Serang
+#define Netral(B) (B).Netral
 
+/* ************** Prototype ************** */
+void MakeBangunan (Bangunan * B, char type, POINT P);
+/* I.S. Sembarane */
+/* F.S. Membuat sebuah bangunan default level 1 (digunakan pada awal load file) */
+
+boolean IsNaikLevel (Bangunan B);
+/* Mengirim True jika bangunan siap naik level */
+
+void NaikLevel (Bangunan *B);
+/* I.S Bangunan valid untuk naik level (bisa naik level) */
+/* F.S Bangunan naik 1 level dengan pertambahan 
+        pasukan sesuai dengan levelnya */
+
+void AddNextTurn (Bangunan * B);
+/* I.S Bangunan terdefinisi */
+/* F.S Bangunan betambah pasukannya sesuai level seteah turn */
+
+void PrintBangunan (Bangunan B);
+/* I.S Sembarang */
+/* F.S Tercetak di layar info bangunannya */
 
 #endif
