7186fa86d079030ecc767f30534cb432f0659114
mzunana <zunanalfikri@gmail.com>
2019-10-29 13:56:37 +0700

    adt

--- TUBES/arraydin_bangunan_driver.c
+++ TUBES/arraydin_bangunan_driver.c
@@ -6,6 +6,7 @@
 
 int main(){
     TabInt T;
+    TabInt T2, T3, T4;
     MakeEmptyArray(&T, 10);
     POINT P;
     printf("lalala");
@@ -18,5 +19,21 @@ int main(){
     for (int i = GetFirstIdx(T) ; i<= GetLastIdx(T) ; i++) {
         PrintBangunan(Elmt(T,i));
     }
+    printf("lalalalala\n");
+    T2 = T;
+
+    for (int i = GetFirstIdx(T) ; i<= GetLastIdx(T) ; i++) {
+        PrintBangunan(Elmt(T2,i));
+    }
+    printf("lalala");
+    T3 = T;
+    for (int i = GetFirstIdx(T) ; i<= GetLastIdx(T) ; i++) {
+        PrintBangunan(Elmt(T3,i));
+    }
+    T4 = T;
+    for (int i = GetFirstIdx(T) ; i<= GetLastIdx(T) ; i++) {
+        PrintBangunan(Elmt(T4,i));
+    }
+    printf("%d %d %d %d", Neff(T), Neff(T2), Neff(T3), Neff(T4));
     return 0;
 }
---
+++ TUBES/stackt.c
@@ -0,0 +1,44 @@
+#include "stackt.h"
+
+
+/* *** Konstruktor/Kreator *** */
+void CreateEmptyStack (Stack *S)
+/* I.S. sembarang; */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxElStack */
+/* jadi indeksnya antara 1.. MaxElStack+1 karena 0 tidak dipakai */
+/* Ciri stack kosong : TOP bernilai Nil */
+{
+    Top(*S) = 0;
+}
+
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmptyStack (Stack S)
+/* Mengirim true jika Stack kosong: lihat definisi di atas */
+{
+    return (Top(S) == 0);
+}
+boolean IsFullStack (Stack S)
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+{
+    return (Top(S) == MaxElStack);
+}
+
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+void Push (Stack * S, infotype X)
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+{
+        Top(*S)++;
+        InfoTop(*S) = X;
+}
+
+/* ************ Menghapus sebuah elemen Stack ************ */
+void Pop (Stack * S, infotype* X)
+/* Menghapus X dari Stack S. */
+/* I.S. S  tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+{
+    *X = InfoTop(*S);
+    Top(*S)--;
+}
