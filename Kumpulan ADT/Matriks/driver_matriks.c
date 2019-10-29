3a18d5ba56255b72a837e6574e9017fe51e71e74
mzunana <zunanalfikri@gmail.com>
2019-10-29 10:00:53 +0700

    adt

--- Kumpulan ADT/Matriks/driver_matriks.c
+++ Kumpulan ADT/Matriks/driver_matriks.c
@@ -11,14 +11,18 @@ int main ()
     /* ALGORITMA */
     printf("Input M1 :\n");
     scanf("%d %d",&i,&j);
-    BacaMATRIKS(&M1,i,j);
+    //BacaMATRIKS(&M1,i,j);
     //printf("Input M2 :\n");
     //scanf("%d %d",&i,&j);
     //BacaMATRIKS(&M2,i,j);
 
+    MakeMATRIKS(i, j, &M1);
     printf("M1 :\n");
     TulisMATRIKS(M1);
     printf("\n");
+    Elmt(M1, 1, 1) = 'V';
+    Elmt(M1, 1, 2) = 'V';
+    Elmt(M1, 1, 3) = 'V';
     //printf("M2 :\n");
     //TulisMATRIKS(M2);
     //printf("\n");
