8704a446c3e673d88aafe64516eed9ab0f7181e8
mzunana <zunanalfikri@gmail.com>
2019-11-01 13:54:21 +0700

    state

--- TUBES/state.h
+++ TUBES/state.h
@@ -11,6 +11,19 @@ typedef struct {
     Player Player2;    
 } State;
 
+/* *** Selektor *** */
+#define ArrayBangunan(S) (S).ArrayBangunan
+#define Player1(S) (S).Player1
+#define Player2(S) (S).Player2
+
+void MakeState(State *S);
+/* I.S. State S Sembarang */
+/* F.S State S telah siap diisi */
+
+void CopyState(State Sin, State * Sout);
+/* I.S. State Sout sembarang */
+/* F.S. State Sout terisi salinan state Sin */
+
 
 
 
