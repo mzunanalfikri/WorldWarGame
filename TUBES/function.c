0a9ef1ea7760484972d7337ef28916a557828dc0
gilliantuerah <gilliantuerah@gmail.com>
2019-11-14 13:09:08 +0700

    nambah func.c

---
+++ TUBES/function.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function.h"
+
+
+/* **** SKILL **** */
+void InstantUpgrade (State *S);
+
+void Shield (State *S); //bonus
+
+void ExtraTurn (State *S);
+
+void AttackUp (State *S); //bonus
+
+void CriticalHit (State *S);
+
+void InstantReinforcement (State *S);
+
+void Barrage (State *S);
+
+/* **** ATTACK MECHANISM *** */
+
