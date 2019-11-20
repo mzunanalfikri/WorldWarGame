830777374f62ab33402b13c938c26f88c0794a37
Muhammad Ayyub Abdurrahman <49425375+Ayyub29@users.noreply.github.com>
2019-11-20 21:18:41 +0700

    function driver beres
    cek lagi dong ada yang kurang ga

--- TUBES/function_drv.c
+++ TUBES/function_drv.c
@@ -41,32 +41,117 @@ void SetSkill(State *S){
         Add(&Q,7);
     }
     PrintQSkill(Q);
+    PrintTopQSkill(Q);
 }
 
-void SetBangunan (Bangunan *B)
-/* I.S. Sembarang */
-/* F.S. Membuat sebuah bangunan default level 1 (digunakan pada awal load file) */
+ void SetBangunan (Bangunan *B)
 {
     int lv,serang,pertahanan,netral,move,pasukan,A,M;
     char type;
+    printf("level: ");
     scanf("%d",&lv);
     Level(*B) = lv;
+    printf("serang?(1/0): ");
     scanf("%d",&serang);
     Serang(*B) = serang;
+    printf("pertahanan?(1/0): ");
     scanf("%d",&pertahanan);
     Pertahanan(*B) = pertahanan;
+    printf("Tipe bangunan? :");
     scanf(" %c",&type);
     Type(*B) = type;
+    printf("Netral(1/0)? :");
     scanf("%d",&netral);
     Netral(*B) = netral;
+    printf("Move( 1/0)? :");
     scanf("%d",&move);
     Move(*B) = move;
+    printf("pasukan? ");
     scanf("%d",&pasukan);
     Pasukan(*B) = pasukan;
+    printf("A? :");
     scanf("%d",&A);
     A(*B) = A;
+    printf("M? :");
     scanf("%d",&M);
     M(*B) = M;
+}
+
+void PrintPlayerBangunan(State S, int *manipulate, boolean player1){
+    int list[31];
+    int temp;
+    addresslist P;
+    int count;
+
+    printf("Daftar Bangunan : \n");
+    P = First(ListIdxBangunan(Player2(S)));
+    if (player1){
+        P = First(ListIdxBangunan(Player1(S)));
+    }
+    count = 1;
+    while (P != NULL){
+        printf("%d. ", count);
+        PrintBangunan(ElmtTab(ArrayBangunan(S), Info(P)));
+        list[count] = Info(P);
+        count++;
+        P = Next(P);
+    }
+    if (count == 1){
+        printf("Tidak ada bangunan yang kamu punya!. \n");
+        *manipulate = 0;
+    } else {
+        printf("Bangunan yang ingin dimanipulasi : ");
+        //scanf("%d", &temp);
+        ReadCmd();
+        temp = KataToInt(CKata);
+        while (temp >= count || temp <= 0){
+            printf ("Masukan salah!\nBangunan yang digunakan untuk dimanipulasi :");
+            //scanf("%d", &temp);
+            ReadCmd();
+            temp = KataToInt(CKata);
+        }
+        *manipulate = list[temp];
+    }
+}
+
+void SetPlayersBangunan(State *S){
+    int n;
+    infotype i;
+    int x;
+    if (Turn(Player1(*S))){
+        while (!IsEmptyList(ListIdxBangunan(Player1(*S)))){
+            DelVFirst(&ListIdxBangunan(Player1(*S)), &i);
+        }
+    } else if (Turn(Player2(*S))) {
+        while (!IsEmptyList(ListIdxBangunan(Player2(*S)))){
+            DelVFirst(&ListIdxBangunan(Player2(*S)), &i);
+        }
+    }
+    printf("Mau berapa bangunan? ");
+    scanf("%d",&n);
+    
+    while (n > 0){
+        printf("masukkan indeks bangunan: ");
+        scanf("%d",&x);
+        if (Turn(Player1(*S))){
+            InsVLast(&ListIdxBangunan(Player1(*S)), x);
+            if (SearchB(ListIdxBangunan(Player2(*S)),x)){
+                DelP(&ListIdxBangunan(Player2(*S)), x);
+            }
+        } else if (Turn(Player2(*S))) {
+            InsVLast(&ListIdxBangunan(Player2(*S)), x);
+            if (SearchB(ListIdxBangunan(Player1(*S)),x)){
+                DelP(&ListIdxBangunan(Player1(*S)), x);
+            }
+        }
+        n--;
+    }
+}
+
+void SetLvBangunanTo4(State *S){
+    InstantUpgrade(S);
+    InstantUpgrade(S);
+    InstantUpgrade(S);
 }
 
 int main() {
@@ -80,11 +165,15 @@ int main() {
     boolean extraTurn;
     boolean attackUp;
     boolean criticalHit;
+    boolean player1;
+    endgame = false;
+    int X;
 
     criticalHit = false;
     extraTurn = false;
     attackUp = false;
     endgame = false;
+    player1 = Turn(Player1(S));
     ReadKonfigurasiFile(&S, &Map, &G);
     Turn(Player1(S)) = true;
     CreateEmptyStackState(&SStacks);
@@ -111,7 +200,18 @@ int main() {
             EndTurnState(&SStacks);
         } else if (IsEQCKataString("SET_SKILL")){
             //manggil fungsi skill
-            SetSkill(&S); 
+            SetSkill(&S);
+        } else if (IsEQCKataString("SET_BANGUNAN")){
+            //manggil fungsi skill
+            PrintPlayerBangunan(S,&X,player1);
+            SetBangunan(&(ElmtTab(ArrayBangunan(S),X)));
+        } else if (IsEQCKataString("SET_PLAYERS_BANGUNAN")){
+            //manggil fungsi skill
+            PrintAllBangunan(ArrayBangunan(S));
+            SetPlayersBangunan(&S);
+        } else if (IsEQCKataString("SET_BANGUNAN_LV4")){
+            //manggil fungsi skill
+            SetLvBangunanTo4(&S);  
         } else if (IsEQCKataString("UNDO")) {
             printf("Undo \n");
             Undo(&SStacks);
@@ -143,6 +243,8 @@ int main() {
             printf("tampilin help");
         } else if (IsEQCKataString("PRINT_GRAPH")) {
             PrintInfoGraph(G);
+        } else if (IsEQCKataString("CEK_WIN")) {
+            GameEnd(S, &endgame);
         }
         //cek juga ada yang kalah atau enggak
     }
