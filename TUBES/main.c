0af9c6e699e55c9a3514000dbc53ff410862d23e
gimhafnu <nifgana@gmail.com>
2019-11-22 10:25:51 +0700

    Save (load belum)
    Belum bisa ganti savepath, cuman state teratas dulu,
    load-nya otw

--- TUBES/main.c
+++ TUBES/main.c
@@ -17,6 +17,7 @@
 #include "mesinkar.h"
 #include "mesinkata.h"
 #include "readfile.h"
+#include "save_load.h"
 // #include "arraydin.c"
 // #include "bangunan.c"
 // #include "listlinier.c"
@@ -265,6 +266,8 @@ int main() {
             Help();
         } else if (IsEQCKataString("PRINT_GRAPH")) {
             PrintInfoGraph(G);
+        } else if (IsEQCKataString("SAVE")) {
+            save(SStacks, Map, G, extraTurn, attackUp, "p.txt");
         } else{
             printf("COMMAND yang anda masukkan tidak tersedia, coba lagi!\n");
         }
